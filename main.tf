b7d34642256bdfe3e570f7e3721bf7b32c6f68b7
selton <sl1586321@gmail.com>
2023-04-25 11:37:20 -0300

    primeira instancia

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0db245b76e5c21ca1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Primeira instancia"
+  }
+}
