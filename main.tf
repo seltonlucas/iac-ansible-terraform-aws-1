c4b62e26ecd34821f35b48a95276495b0cf7af96
selton <sl1586321@gmail.com>
2023-04-26 09:35:03 -0300

    User_data ad hoc commands

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0db245b76e5c21ca1"
   instance_type = "t2.micro"
+  key_name      = "iac-alura"
+  user_data     = <<-EOF
+              #!/bin/bash
+              cd /home/ubuntu
+              echo "<h1>Criado pelo Terraform TM</h1>" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF 
 
   tags = {
     Name = "Primeira instancia"
