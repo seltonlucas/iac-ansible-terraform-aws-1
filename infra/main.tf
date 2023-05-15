893a13b192b41e27f8e9aa8c8a39d400e430752a
Selton Lucas <sl1586321@gmail.com>
2023-05-15 17:42:58 -0300

    Creating load balancer

--- infra/main.tf
+++ infra/main.tf
@@ -31,12 +31,47 @@ resource "aws_key_pair" "chaveSSH" {
 }
 
 resource "aws_autoscaling_group" "grupo" {
-  availability_zones = [ "${var.regiao_aws}a" ]
+  availability_zone = [ "${var.regiao_aws}a" ]
   name = var.nomeGrupo
   max_size = var.maximo
   min_size = var.minimo
   launch_template {
     id = aws_launch_template.maquina.id
     version = "$latest"
+  }
+  target_group_arns = [ aws_lb_target_group.alvoLoadBalancer.arn ]
+}
+
+resource "aws_default_subnet" "subnet_1" {
+  availability_zone = [ "${var.regiao_aws}a" ]
+}
+
+resource "aws_default_subnet" "subnet_2" {
+  availability_zone = [ "${var.regiao_aws}b" ]
+}
+
+resource "aws_lb" "loadBalancerProducao" {
+  internal = false
+  subnets = [ aws_default_subnet.subnet_1, aws_default_subnet.subnet_2 ]
+}
+
+resource "aws_lb_target_group" "alvoLoadBalancer" {
+  name = "maquinasAlvo"
+  port = "8000"
+  protocol = "HTTP"
+  vpc_id = aws_default_vpc.default.id
+}
+
+resource "aws_default_vpc" "default" {
+  
+}
+
+resource "aws_lb_listener" "entradaLoadBalancer" {
+  load_balancer_arn = aws_lb.loadBalancerProducao.arn
+  port = "8000"
+  protocol = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.alvoLoadBalancer.arn
   }
 }
